040e0009920ea151969d4d305610613d9dffb1fb
MY PC <yemgold@gmail.com>
2026-06-29 14:00:22 +0100

    new update

--- src/hooks/checkOut/useCheckout.ts
+++ src/hooks/checkOut/useCheckout.ts
@@ -5,7 +5,7 @@ import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
 
 import { checkoutService } from "../../services/checkout.service";
-import { useCart } from "../cart/useCartPublick";
+import { useCart } from "../cart/useCart";
 import { useAuthStore } from "../../store/auth.store";
 
 export function useCheckout() {
